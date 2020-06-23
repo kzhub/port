a7fe3cace3f0e11ee3dd334e4061e5abb1d4fb8d
kzhub <55772750+kzhub@users.noreply.github.com>
2020-06-23 22:18:00 +0900

    Add files via upload

---
+++ KazukiKoike/script.js
@@ -0,0 +1,121 @@
+function pen(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function pena(){
+  let modal= document.getElementById("modal2");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penb(){
+  let modal= document.getElementById("modal3");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penc(){
+  let modal= document.getElementById("modal4");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function pend(){
+  let modal= document.getElementById("modal5");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function pene(){
+  let modal= document.getElementById("modal6");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penf(){
+  let modal= document.getElementById("modal7");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function peng(){
+  let modal= document.getElementById("modal8");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penh(){
+  let modal= document.getElementById("modal9");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function peni(){
+  let modal= document.getElementById("modal10");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penj(){
+  let modal= document.getElementById("modal11");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penk(){
+  let modal= document.getElementById("modal12");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function penl(){
+  let modal= document.getElementById("modal13");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+}
+
+function pen3(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'none';
+  let modalw= document.getElementById("modal2");
+  modalw.style.display = 'none';
+  let modala= document.getElementById("modal3");
+  modala.style.display = 'none';
+  let modalb= document.getElementById("modal4");
+  modalb.style.display = 'none';
+  let modalc= document.getElementById("modal5");
+  modalc.style.display = 'none';
+  let modald= document.getElementById("modal6");
+  modald.style.display = 'none';
+  let modale= document.getElementById("modal7");
+  modale.style.display = 'none';
+  let modalf= document.getElementById("modal8");
+  modalf.style.display = 'none';
+  let modalg= document.getElementById("modal9");
+  modalg.style.display = 'none';
+  let modalh= document.getElementById("modal10");
+  modalh.style.display = 'none';
+  let modali= document.getElementById("modal11");
+  modali.style.display = 'none';
+  let modalj= document.getElementById("modal12");
+  modalj.style.display = 'none';
+  let modalk= document.getElementById("modal13");
+  modalk.style.display = 'none';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'none';
+}
